73b47a1b7c244c0c89959f2262591cc9ac16f456
wayson14 <wayson.1104@gmail.com>
2022-02-04 17:55:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Chat from './components/Chat.js'
+import Hub from './components/Hub.js'
+import Login from './components/Login.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+        <Link to="/chat">chat</Link>
+        <Link to="/hub">hub</Link>
+        <Link to="/login">login</Link>
+        <div className='main'>
+          <Routes>
+            <Route path="/chat" element={<Chat/>}/>
+            <Route path="/hub" element={<Hub/>}/>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </div>
+
+      </Router>
       </header>
+     
     </div>
   );
 }
---
+++ src/components/Hub.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hub = () => {
+  return <div>hub</div>;
+};
+
+export default Hub;
